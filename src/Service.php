5bf9f54da52c128ba7c5a7858743d9f731d73589
hackyoung <hackyoung@163.com>
2016-05-05 18:49:58 +0800

    add Service

--- src/Service.php
+++ src/Service.php
@@ -3,11 +3,15 @@ namespace \Leno;
 
 class Service
 {
-    public static function getService()
-    {
-    }
+    protected static $base = 'model.service';
 
-    public static function reigsterNameSpace()
+    public static function getService($service_name, $args)
     {
+        $name = self::$base . '.' . $service_name;
+        $class = preg_replace_callback('/^\w|\.\w/', function($matches) {
+            return strtoupper(str_replace('.', '', $matches[0]));
+        }, $name);
+        $reflector = new \ReflectionClass($class);
+        return $reflector->newInstanceArgs($args);
     }
 }
