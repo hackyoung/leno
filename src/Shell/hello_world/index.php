a9d24190a52959cff1c958c183b3dd9cc3f20393
hackyoung <hackyoung@163.com>
2016-05-21 08:21:53 +0800

    fixed hello world

--- src/Shell/hello_world/index.php
+++ src/Shell/hello_world/index.php
@@ -1,16 +1,26 @@
 <?php
+// 定义ROOT变量
 define('ROOT', __DIR__);
 
 require_once ROOT .'/vendor/autoload.php';
 
+// 注册名字空间
 \Leno\AutoLoader::register('Model', '/model');
 \Leno\AutoLoader::register('Controller', '/controller');
 
+// 添加view目录
 \Leno\View::addViewDir(ROOT . '/view');
 
+// 设置view的缓存目录
 \Leno\View\Template::setCacheDir(ROOT . '/tmp/view');
 
+// 设置router
 Worker::setRouterClass('\\Router');
 
-$worker = Worker::instance()->errorToException();
+$worker = Worker::instance();
+
+// 将所有的错误转换为异常,如果不需要开启此功能则注释掉这行代码
+$worker->errorToException();
+
+// 开始执行worker
 $worker->execute();
