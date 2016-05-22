e7cb1eef98c204de35fad89c84f3f7eb60c2ffc4
hackyoung <hackyoung@163.com>
2016-05-23 04:40:04 +0800

    fixed bug

--- src/Shell/hello_world/view/index.lpt.php
+++ src/Shell/hello_world/view/index.lpt.php
@@ -1,5 +1,12 @@
 <extend name="_layout.default">
     <fragment name="body">
-        <center>{$hello}</center>
+        <style>
+        .hello {
+            font-size: 20px;
+            margin-top: 100px;
+            text-shadow: 3px 3px 3px rgba(0, 0, 0, 0.8);
+        }
+        </style>
+        <center class="hello">{$hello}</center>
     </fragment>
 </extend>
