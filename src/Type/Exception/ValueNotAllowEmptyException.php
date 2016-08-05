d0993c90a94876406b6607ad299a72fff3c9956d
hackyoung <hackyoung@163.com>
2016-08-05 15:23:14 +0800

    fixed bug

--- src/Type/Exception/ValueNotAllowEmptyException.php
+++ src/Type/Exception/ValueNotAllowEmptyException.php
@@ -4,10 +4,4 @@ namespace Leno\Type\Exception;
 class ValueNotAllowEmptyException extends \Leno\Type\Exception
 {
     protected $messageTemplate = '%s Not Allow Empty';
-
-    public function __construct($name, $val)
-    {
-        $message = sprintf('%s[current value: %s]', $name, $val);
-        parent::__construct($message);
-    }
 }
