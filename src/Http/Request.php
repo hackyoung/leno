c849915c1063d7cfc71cb31cc5a34e72cbbef193
hackyoung <hackyoung@163.com>
2016-09-02 11:35:53 +0800

    fixed bug

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -5,6 +5,8 @@ class Request extends \GuzzleHttp\Psr7\Request
 {
     private $attribute = [];
 
+    private $ip;
+
     public function input()
     {
         return file_get_contents('php://input', 'r');
@@ -26,6 +28,20 @@ class Request extends \GuzzleHttp\Psr7\Request
 
     public function hasAttribute($attr) {
         return isset($this->attribute[$attr]);
+    }
+
+    function getIp()
+    {
+        if ($this->ip) {
+            return $this->ip;
+        } elseif (getenv("HTTP_CLIENT_IP")) {
+            $this->ip = getenv("HTTP_CLIENT_IP");
+        } elseif(getenv("HTTP_X_FORWARDED_FOR")) {
+            $this->ip = getenv("HTTP_X_FORWARDED_FOR");
+        } elseif(getenv("REMOTE_ADDR")) {
+            $this->ip = getenv("REMOTE_ADDR");
+        }
+        return $this->ip;
     }
 
     public static function getNormal()
